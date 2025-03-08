10a5f761c3abfa09c88554f52500cd549e27261a
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2025-03-09 00:43:48 +0500

    trash pages removed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,20 @@
+'use client';
+
+import { ROUTER } from '@/constants/router';
+import { useUserStore } from '@/store/user';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 const Page = () => {
+    const { hasAccess } = useUserStore(state => state);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!hasAccess) {
+            router.push(ROUTER.LOGIN);
+        }
+    }, [hasAccess, router]);
+
     return <div>ADMIN HOME PAGE</div>;
 };
 
